e570ff43800a1f3ccff0c87b83711b1ef5f703d4
Michal <michalek99_9@tlen.pl>
2025-04-24 15:03:35 +0200

    typo corrected

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const notesController = require("../controllers/NotesController");
+const notesController = require("../controllers/notesController");
 const verifyJwt = require("../middleware/verifyJwt");
 
 router.use(verifyJwt);
